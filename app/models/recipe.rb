05e08992f220c21dd029f6b743b61ab39749482e
gionaufal <g10v45+github@gmail.com>
2016-09-26 08:46:59 -0300

    user uploads image in recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, :cuisine_id, :kind, :ingredients, :steps, presence: true
   belongs_to :cuisine
-  has_attached_file :image
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
